741ab1656764bc296ea9699957fc2cfbde74c366
Jayathunga T.M <thiroshmadhusha0520@gmail.com>
2023-06-24 12:42:29 +0530

    Add Volume ToggleButton

--- text_to_speech/lib/main.dart
+++ text_to_speech/lib/main.dart
@@ -73,25 +73,81 @@ class _MyAppState extends State<MyApp> {
             )));
   }
 
+  // *******************************************************
+
+  // Add toggle button to input widget
+  void toggleButtonPressed() {
+    setState(() {
+      if (_ttsState == TtsState.playing) {
+        stop();
+      } else {
+        speak();
+      }
+    });
+  }
+
   // input TTS text field
   Widget input() {
     return Container(
       alignment: Alignment.topCenter,
       padding: const EdgeInsets.all(25.0),
-      child: TextField(
-        onChanged: (String value) {
-          setState(() {
-            _tts = value;
-          });
-        },
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Capture The Page Text',
-        ),
-        maxLines: null, // Allows for multiline input
+      child: Stack(
+        alignment:
+            Alignment.topRight, // Align toggle button to top-right corner
+        children: [
+          TextField(
+            onChanged: (String value) {
+              setState(() {
+                _tts = value;
+              });
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Capture The Page Text',
+            ),
+            maxLines: null, // Allows for multiline input
+          ),
+          Padding(
+            padding: const EdgeInsets.all(
+                8.0), // Add padding around the toggle button
+            child: ElevatedButton(
+              onPressed: toggleButtonPressed,
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(8.0),
+                primary: Colors.grey[300],
+              ),
+              child: Icon(
+                _ttsState == TtsState.playing
+                    ? Icons.volume_up
+                    : Icons.volume_off,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  // Widget input() {
+  //   return Container(
+  //     alignment: Alignment.topCenter,
+  //     padding: const EdgeInsets.all(25.0),
+  //     child: TextField(
+  //       onChanged: (String value) {
+  //         setState(() {
+  //           _tts = value;
+  //         });
+  //       },
+  //       decoration: const InputDecoration(
+  //         border: OutlineInputBorder(),
+  //         labelText: 'Capture The Page Text',
+  //       ),
+  //       maxLines: null, // Allows for multiline input
+  //     ),
+  //   );
+  // }
 
   // Widget input() => Container(
   //       alignment: Alignment.topCenter,
@@ -105,33 +161,31 @@ class _MyAppState extends State<MyApp> {
   //       ),
   //     );
 
-
   // *******************************************************
 
   // play and stop button
 
   Widget button() {
-  return Container(
-    margin: const EdgeInsets.symmetric(vertical: 20.0),
-    child: ElevatedButton(
-      onPressed: _ttsState == TtsState.stopped ? speak : stop,
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-          side: const BorderSide(color: Colors.black),
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20.0),
+      child: ElevatedButton(
+        onPressed: _ttsState == TtsState.stopped ? speak : stop,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            side: const BorderSide(color: Colors.black),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            _ttsState == TtsState.stopped ? 'Play' : 'Stop',
+            style: const TextStyle(fontSize: 18.0),
+          ),
         ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          _ttsState == TtsState.stopped ? 'Play' : 'Stop',
-          style: const TextStyle(fontSize: 18.0),
-        ),
-      ),
-    ),
-  );
-}
-
+    );
+  }
 
   // Widget button() {
   //   if (_ttsState == TtsState.stopped) {
